60e2053575513ed20e27616bb1c037f637c21f61
MeszarosEtele <77536225+meszarosetele@users.noreply.github.com>
2022-05-01 16:43:08 +0200

    Menu: Cimlap

---
+++ templates/pages/cimlap.tpl.php
@@ -0,0 +1,23 @@
+<header>                              
+						<h1 class="entry-title page-header">
+							Küldetés						</h1> 
+						<time class="posted-on published" datetime="2017-08-08"></time>                                                        
+					</header>                            
+					<div class="entry-content">                              
+						<p><b><span style="font-size: 14.0pt; font-family: 'Arial','sans-serif'; color: black;">Az alapítvány küldetése</span></b></p>
+<p><span style="font-size: 14.0pt; font-family: 'Arial','sans-serif'; color: black;">A Drogprevenciós Alapítvány az 1994-es megalapításakor a hiteles, objektív tényeket tartalmazó információnyújtásra helyezte a hangsúlyt. Az iskolákat járva primer prevenciós foglakozásokat tartott diákok számára a viselkedéses-, és a legális és illegális kémiai addikciók témakörében egyaránt.</span></p>
+<p><span style="font-size: 14.0pt; font-family: 'Arial','sans-serif'; color: black;">Mivel tudtuk, hogy az elsődleges megelőzés nem elégséges azok számára, akik nem tudnak és/vagy nem akarnak változtatni szerhasználati szokásukon, így 1995-ben megalakítottuk az alacsonyküszöbű, ártalomcsökkentő tűcsereprogramunkat is. Azok, akik nem kerestek fel bennünket, azok számára az utcai megkereső munka volt az egyetlen üdvözítő módszer, így 1996-ban majd 1998- 2011 között rendszeresen kijártunk a kilencedik kerületi Dzsumbujba is. </span></p>
+<p><span style="font-size: 14.0pt; font-family: 'Arial','sans-serif'; color: black;"> Tevékenységünkkel nem csupán a pszichoaktív szereket használó klienseknek és a hozzátartozóiknak, környezetüknek akartunk segíteni, hanem célul tűztük ki az ártalomcsökkentés filozófiájának és gyakorlatának meghonosítását is. </span></p>
+<p><span style="font-size: 14.0pt; font-family: 'Arial','sans-serif'; color: black;">A drogprevenciós foglakozások és a tűcsere program működtetése közben szerzett tapasztalataink folyamatosan hívták életre az összes többi projektünket. Így bővült a bio-pszicho-szociális ellátásunk az ambuláns ellátással. Ötvözzük a konzultációt és a szubsztitúciós kezelést. Ez azon az elven alapszik, hogy a klienseknek a gyógyszeres kezelés mellett, a széleskörű problémáik feltárásában és a megrekedt élethelyzetük előremozdításában lehetőség szerint a családjukkal együttműködve egy szakavatott team képes segíteni.</span></p>
+<p><span style="font-size: 14.0pt; font-family: 'Arial','sans-serif'; color: black;">17 fős stábunk működteti az egymásra épülő programjainkat. Így az utcai munka mellett a fix telephelyű alacsonyküszöbű programban és az egészségfejlesztésben, a szexuális felvilágosításban, ön- és társismeret fejlesztésében és a közművelődésben az ambuláns ellátásban és a rehabilitációra való felkészítésben is szerepet vállalunk.</span></p>
+<p><i><span style="font-size: 14.0pt; font-family: 'Arial','sans-serif'; color: black;">Célunk</span></i><span style="font-size: 14.0pt; font-family: 'Arial','sans-serif'; color: black;">, hogy a hozzánk fordulók kapják meg a számukra legalkalmasabb ellátást, információt. Klienseink körébe a droghasználók és hozzátartozóik, illetve diákok, szociális segítők, egészségügyi dolgozók, pedagógusok tartoznak.</span></p>
+<p><span style="font-size: 14.0pt; font-family: 'Arial','sans-serif'; color: black;">Az 1994 óta működő alapítványunk tudja, hogy egy-egy szervezet önmagában nem érhet el minden érintettet, és nem fedhet le minden területet tevékenységével. A klienseink megfelelő ellátása miatt is fontosnak tartjuk a szervezetek közötti gyakorlati <i>együttműködést.</i></span></p>
+<p><span style="font-size: 14pt;"><a href="http://drogprevenciosalapitvany.hu/magunkrol/"><span style="font-family: Arial, sans-serif; color: black;"><i>Az alapítványról&#8230;</i></span></a></span></p>
+<p>&nbsp;</p>
+                            
+					</div> 
+
+<div class="mediabox1">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/HRUVLix3eI8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<video width="320" height="240" controls> <source src="videos/dijatado.mp4" type="video/mp4"> Az ön böngészője nem támogatja a beágyazott videókat! </video>
+</div>
